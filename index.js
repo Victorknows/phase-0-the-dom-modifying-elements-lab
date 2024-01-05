a8c9a226cd11b779bf4b7e78b77b4efda1bb9035
Victor Muhoro <victormuhoro7@gmail.com>
2024-01-05 15:28:45 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+var remove = document.getElementById('main');
+remove.remove();
+
+var newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Victor is the champion';
+
